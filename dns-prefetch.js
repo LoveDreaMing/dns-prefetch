f4307d9ed273bafc6cb01fd2034a2e737a29d98a
huangjiaming04 <huangjiaming04@58.com>
2023-10-10 17:25:00 +0800

    feat: copy from duyi

--- dns-prefetch.js
+++ dns-prefetch.js
@@ -1,36 +1,51 @@
-const path = require('path');
-const fs = require('fs');
-const filePath = path.resolve(__dirname, './dist');
-const suffixs = ['.html', '.js', '.css'];
+const fs = require('fs'); // node文件系统
+const { parse } = require('node-html-parser'); // html解析器，生成一个简化的dom树
+const { glob } = require('glob'); // 使用 shell 使用的模式匹配文件
+const urlRegex = require('url-regex'); // 用于匹配网址的正则表达式
 
-const set = new Set();
-let template = '';
-readFile(filePath);
-function readFile(way) {
-    const files = fs.readdirSync(way);
-    files.forEach(file => {
-        const filedir = path.join(way, file);
-        const stats = fs.statSync(filedir);
-        const isContinue = suffixs.includes(path.extname(filedir));
-        if (stats.isFile() && isContinue) {
-            const data = fs.readFileSync(filedir);
-            const str = data.toString();
-            if (path.basename(filedir) === 'index.html') {
-                template = str;
-            }
-            const reg = /\/\/([a-z0-9.]+)(.com|.cn){1}/ig;
-            str.match(reg).forEach(item => {
-                set.add(item);
+const urlPattern = /(\/\/[^\/]*)/i; // url正则表达式
+const urls = new Set(); // 存储域名地址
+
+/**
+ * @name 检索域名
+ * @description 在“dist”目录及其子目录中搜索扩展名为 html、css 和 js 的文件，读取每个文件并使用正则表达式匹配URL，收集所有URL
+ */
+async function searchDomain() {
+    const files = await glob('dist/**/*.{html,css,js}'); // 获取html、css、js文件
+    for (const file of files) {
+        const source = fs.readFileSync(file, 'utf-8'); // 读取文件
+        const matches = source.match(urlRegex({ strict: true })); // 匹配url
+        if (matches) {
+            matches.forEach((url) => {
+                const match = url.match(urlPattern); // 匹配url
+                if (match && match[1]) {
+                    urls.add(match[1]); // 收集url
+                }
             });
         }
-        if (stats.isDirectory()) {
-            readFile(filedir);
-        }
-    });
+    }
 }
-let all = '';
-for (let x of set) {
-    all += `<link rel="dns-prefetch" href="${x}">`
+
+/**
+ * @name 插入链接
+ * @description 将DNS预取链接插入HTML文件
+ */
+async function insertLinks() {
+    const files = await glob('dist/**/*.html'); // 获取html文件
+    const links = [...urls]
+        .map((url) => `<link rel="dns-prefetch" href="${url}">`)
+        .join('\n'); // 生成dns预取链接
+    for (const file of files) {
+        const html = fs.readFileSync(file, 'utf-8'); // 读取文件
+        const root = parse(html); // 解析html，生成一个简化的dom树
+        const head = root.querySelector('head'); // 获取head标签
+        head.insertAdjacentHTML('afterbegin', links); // 插入dns预取链接
+        fs.writeFileSync(file, root.toString()); // 写入文件
+    }
 }
-template = template.split('<title>').join(all + '<title>');
-fs.writeFileSync('./dist/index.html', template);
+
+async function main() {
+    await searchDomain(); // 检索域名
+    await insertLinks(); // 插入链接
+}
+main();
